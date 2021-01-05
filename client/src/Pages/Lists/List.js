84ab6510e6e0559b744dedb5f7d22455af44dfc5
Sergio Flores <byoigres@gmail.com>
2021-01-05 13:32:31 -0700

    Removed commented code

--- client/src/Pages/Lists/List.js
+++ client/src/Pages/Lists/List.js
@@ -12,38 +12,6 @@ import {
   ItemAuthor,
   VideoListItemActions,
 } from "../../components/VideoListItem";
-
-// const ItemContainer = styled.div`
-//   display: flex;
-//   align-items: stretch;
-//   flex-grow: 1;
-//   box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
-//   margin-bottom: 1rem;
-//   /* max-height: 100px; */
-// `;
-
-// const ItemImage = styled.img`
-//   background-image: url(${(p) => p.backgroundImage});
-//   align-self: center;
-//   flex: 0 0 auto;
-//   width: 33.3333333333%;
-// `;
-
-// const ItemContent = styled.div`
-//   display: flex;
-//   flex-wrap: wrap;
-//   flex-direction: column;
-//   justify-content: center;
-//   text-align: center;
-// `;
-
-// const ItemTitle = styled.h1`
-//   font-size: 1.4rem;
-// `;
-
-// const ItemAuthor = styled.strong`
-//   font-size: 0.8rem;
-// `;
  
 const List = ({ lists }) => {
   return (
@@ -65,11 +33,6 @@ const List = ({ lists }) => {
                 <ItemAuthor>List by {item.user.email}</ItemAuthor>
               </VideoListItemDescriptions>
             </VideoListItemContent>
-            {/* <VideoListItemActions>
-            <i className="fas fa-trash-alt"></i>
-            <i className="fas fa-arrow-up"></i>
-            <i className="fas fa-arrow-down"></i>
-          </VideoListItemActions> */}
           </VideoListItemContainer>
         ))}
       </Window>
