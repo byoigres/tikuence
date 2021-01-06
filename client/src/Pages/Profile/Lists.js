845edd0d97b36b3bf6b7a5ca1804f9ed16f1ee5d
Sergio Flores <byoigres@gmail.com>
2021-01-06 10:05:17 -0700

    Added Profile/Lists component

---
+++ client/src/Pages/Profile/Lists.js
@@ -0,0 +1,41 @@
+import React, { Fragment } from "react";
+import { Inertia } from "@inertiajs/inertia";
+import Layout from "../../components/Layout"
+import Window from "../../components/Window"
+import {
+  VideoListItemContainer,
+  VideoListItemContent,
+  VideoListItemImage,
+  VideoListItemDescriptions,
+  ItemTitle,
+  ItemAuthor,
+} from "../../components/VideoListItem";
+
+const List = ({ lists }) => {
+  return (
+    <Layout>
+      <Window title="My lists">
+        {lists.map((item) => (
+          <VideoListItemContainer key={item.id}>
+            <VideoListItemContent
+              onClick={(e) => {
+                e.preventDefault();
+                Inertia.replace(`/list/1`);
+              }}
+            >
+              <VideoListItemImage
+                src={`/images/${item.videos[0].thumbnail_name}`}
+              />
+              <VideoListItemDescriptions>
+                <ItemTitle>{item.title}</ItemTitle>
+                <ItemAuthor>List by {item.user.email}</ItemAuthor>
+              </VideoListItemDescriptions>
+            </VideoListItemContent>
+          </VideoListItemContainer>
+        ))}
+      </Window>
+    </Layout>
+  );
+}
+
+export default List;
