0a2c311afbc77f459ef124bb77ed6fb3c2363431
Sergio Flores <byoigres@gmail.com>
2021-01-11 19:09:56 -0700

    Changed  detauls view to a Dialog component

--- client/src/Pages/Lists/Details.jsx
+++ client/src/Pages/Lists/Details.jsx
@@ -3,9 +3,28 @@ import styled from 'styled-components';
 import { Inertia } from '@inertiajs/inertia';
 import { usePage } from '@inertiajs/inertia-react';
 import { TikTok } from 'react-tiktok';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogContent from '@material-ui/core/DialogContent';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
+import Slide from '@material-ui/core/Slide';
+import ArrowBackIcon from '@material-ui/icons/ArrowBack';
 import Layout from '../../components/Layout';
 import TikTokVideo from '../../components/TikTokVideo';
+
+const useStyles = makeStyles((theme) => ({
+  appBar: {
+    position: 'relative',
+  },
+  title: {
+    marginLeft: theme.spacing(2),
+    flex: 1,
+  },
+}));
 
 const Container = styled.section`
   height: 100vh;
@@ -31,33 +50,58 @@ const Video = styled.article`
   /* margin: 1rem 0; */
 `;
 
+/* eslint react/jsx-props-no-spreading: 0 */
+const Transition = React.forwardRef((props, ref) => (
+  <Slide direction="left" ref={ref} {...props} />
+));
+
 const Details = ({ list }) => {
+  const classes = useStyles();
   const { error } = usePage();
   const [isLoading, setIsLoading] = useState(false);
 
+  function handleClose() {
+    Inertia.visit('/');
+  }
+  function handleCreate() {}
+
   return (
-    <Container data-name="Container">
-      <Video key="title">
-        <Typography
-          component="h4"
-          variant="h4"
-          style={{
-            margin: '1rem 1rem 0 1rem',
-            textAlign: 'center',
-          }}
-        >
-          {list.title}
-        </Typography>
-      </Video>
-      {list.videos.map((video) => (
-        <Video key={video.id}>
-          <TikTokVideo html={video.html} />
-        </Video>
-      ))}
-    </Container>
+    <Dialog
+      fullScreen
+      open
+      onClose={handleClose}
+      TransitionComponent={Transition}
+      closeAfterTransition
+    >
+      <AppBar className={classes.appBar}>
+        <Toolbar>
+          <IconButton
+            edge="start"
+            color="inherit"
+            onClick={handleClose}
+            aria-label="close"
+            disabled={isLoading}
+          >
+            <ArrowBackIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            {list.title}
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <DialogContent>
+        <Container data-name="Container">
+          {list.videos.map((video) => (
+            <Video key={video.id}>
+              <TikTokVideo html={video.html} />
+            </Video>
+          ))}
+        </Container>
+      </DialogContent>
+    </Dialog>
   );
 };
 
-Details.layout = (page) => <Layout children={page} title="Welcome" cleanLayout />;
+Details.layout = (page) => <Layout children={page} title={page.props.list.title} cleanLayout />;
 
 export default Details;
