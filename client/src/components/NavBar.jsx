be060dac05b5adcbef1de1dc1d2cb4bb0889be7e
Sergio Flores <byoigres@gmail.com>
2021-01-14 11:25:11 -0700

    Hide add button from bottom navigation when the user is not logged

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -6,7 +6,7 @@ import BottomNavigation from '@material-ui/core/BottomNavigation';
 import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
 import ListIcon from '@material-ui/icons/List';
 import AddBoxIcon from '@material-ui/icons/AddBox';
-import PersonIcon from '@material-ui/icons/Person';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 
 const useStyles = makeStyles({
   bottomNavigation: {
@@ -18,85 +18,50 @@ const useStyles = makeStyles({
   },
 });
 
-/*
-const NavBarContainer = styled.header`
-  position: fixed;
-  z-index: 12;
-  background-color: white;
-  height: 45px;
-  width: 100%;
-  bottom: 0;
-  left: 0;
-  background-color: #000;
-  display: flex;
-  align-items: center;
-`;
-
-const NavBarItem = styled.div`
-  color: #fff;
-  flex: 1;
-  text-align: center;
-  font-size: 2rem;
-  & > a {
-    padding: 0.5rem;
-    color: white;
-  }
-`;
-
-const NavBar2 = () => {
-  const [isCreateListModalOpen, setIsCreateListModalOpen] = useState(false);
-
-  function toggleCreateListModalOpen(e) {
-    setIsCreateListModalOpen(!isCreateListModalOpen);
-  }
-
-  return (
-    <>
-      <NavBarContainer>
-        <NavBarItem>
-          <InertiaLink href="/">
-            <i className="fas fa-list" />
-          </InertiaLink>
-        </NavBarItem>
-        <NavBarItem>
-          <InertiaLink
-            href="#"
-            onClick={(e) => {
-              e.preventDefault();
-              setIsCreateListModalOpen(true);
-            }}
-          >
-            <i className="fas fa-plus-square" />
-          </InertiaLink>
-        </NavBarItem>
-        <NavBarItem>
-          <InertiaLink href="/profile/lists">
-            <i className="fas fa-user" />
-          </InertiaLink>
-        </NavBarItem>
-      </NavBarContainer>
-      <AddPage isOpen={isCreateListModalOpen} closeCallback={toggleCreateListModalOpen} />
-    </>
-  );
-};
-*/
-const NavBar = () => {
+const NavBar = ({ isAuthenticated }) => {
   const classes = useStyles();
   const [selectedAction, setSelectedAction] = React.useState(0);
+
+  const items = [
+    {
+      id: 'lists',
+      component: (
+        <BottomNavigationAction
+          key="lists"
+          label="Lists"
+          icon={<ListIcon data-name="lists-inner" />}
+        />
+      ),
+    },
+    isAuthenticated && {
+      id: 'add-list',
+      component: <BottomNavigationAction key="add-list" label="Add" icon={<AddBoxIcon />} />,
+    },
+    {
+      id: 'profile',
+      component: (
+        <BottomNavigationAction
+          key="profile"
+          label={isAuthenticated ? 'Profile' : 'Login'}
+          icon={<AccountCircleIcon />}
+        />
+      ),
+    },
+  ];
 
   return (
     <BottomNavigation
       value={selectedAction}
       onChange={(_, action) => {
-        switch (action) {
-          case 0:
+        switch (items[action].id) {
+          case 'lists':
             Inertia.get('/');
             break;
-          case 1:
+          case 'add-list':
             Inertia.get(`/list/add?returnUrl=${encodeURIComponent(window.location.pathname)}`);
             break;
-          case 2:
-            Inertia.get('/profile');
+          case 'profile':
+            Inertia.get(isAuthenticated ? '/profile' : '/login');
             break;
           default:
             Inertia.get('/');
@@ -107,9 +72,7 @@ const NavBar = () => {
       showLabels
       className={classes.bottomNavigation}
     >
-      <BottomNavigationAction label="Lists" icon={<ListIcon />} />
-      <BottomNavigationAction label="Add" icon={<AddBoxIcon />} />
-      <BottomNavigationAction label="Profile" icon={<PersonIcon />} />
+      {items.map((x) => x.component)}
     </BottomNavigation>
   );
 };
