2511dd6e0b17ec6c30a1e6b6cb694f841fc31fd5
Sergio Flores <byoigres@gmail.com>
2021-01-15 12:26:21 -0700

    Added Session page

--- client/src/Pages/Auth/Session.jsx
+++ client/src/Pages/Auth/Session.jsx
@@ -1,32 +1,47 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
 import TwitterIcon from '@material-ui/icons/Twitter';
 import GTranslateIcon from '@material-ui/icons/GTranslate';
+import Layout from '../../components/Layout';
 
 const Session = () => (
-  <>
-    <h1>Login</h1>
-    <Button
-      variant="contained"
-      color="secondary"
-      startIcon={<GTranslateIcon />}
-      style={{ backgroundColor: '#E04A32' }}
-      fullWidth
-      href="/auth/google"
+  <div>
+    <Paper
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        flexDirection: 'column',
+        alignItems: 'center',
+        alignSelf: 'center',
+      }}
     >
-      Google
-    </Button>
-    <Button
-      variant="contained"
-      color="secondary"
-      startIcon={<TwitterIcon />}
-      style={{ backgroundColor: '#64CCF1' }}
-      fullWidth
-      href="/auth/twitter"
-    >
-      Twitter
-    </Button>
-  </>
+      <Typography variant="h6">Sign in with yout social account</Typography>
+      <Button
+        variant="contained"
+        color="secondary"
+        startIcon={<GTranslateIcon />}
+        style={{ backgroundColor: '#E04A32' }}
+        fullWidth
+        href="/auth/google"
+      >
+        Google
+      </Button>
+      <Button
+        variant="contained"
+        color="secondary"
+        startIcon={<TwitterIcon />}
+        style={{ backgroundColor: '#64CCF1' }}
+        fullWidth
+        href="/auth/twitter"
+      >
+        Twitter
+      </Button>
+    </Paper>
+  </div>
 );
+
+Session.layout = (page) => <Layout children={page} title="Login" />;
 
 export default Session;
