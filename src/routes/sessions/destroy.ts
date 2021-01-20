2ba122c793d688bebba68eba1c050f0c0b04d9a8
Sergio Flores <byoigres@gmail.com>
2021-01-20 12:13:35 -0700

    Removed unused files

--- src/routes/sessions/destroy.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response } from 'express'
-import { isAuthenticated } from '../../middlewares/inertia'
-
-function destroy(_req: Request, res: Response) {
-  res.clearCookie('ssid')
-  res.redirect('/sessions')
-}
-
-export default [isAuthenticated, destroy]
--- src/routes/sessions/view.ts
+++
@@ -1,9 +0,0 @@
-import { Request } from 'express'
-
-function view(req: Request) {
-  req.Inertia.setViewData({ title: 'Log in' }).render({
-    component: 'Auth/Session'
-  })
-}
-
-export default view
