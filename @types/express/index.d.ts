6039bdab9547daa235e5340cec672f739f905fc2
Sergio Flores <byoigres@gmail.com>
2021-02-10 10:46:40 -0700

    Added User interface definition

--- @types/express/index.d.ts
+++ @types/express/index.d.ts
@@ -2,6 +2,18 @@ import { IInertia } from "inertia-node";
 
 declare global {
   namespace Express {
+    /**
+     * @see https://stackoverflow.com/a/60981984/1301872
+     */
+    interface User {
+      id: number;
+      email: string;
+      provider: {
+        google?: string;
+        twitter?: string;
+        local?: string;
+      }
+    }
     export interface Request {
       auth: {
         isAuthenticated: boolean;
