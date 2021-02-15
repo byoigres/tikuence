e42b724b3a90a425f1cd3c850da860a0f103a4d8
Sergio Flores <byoigres@gmail.com>
2021-02-15 12:28:04 -0700

    Added 404 page when an image not exists

--- src/routes/images.ts
+++ src/routes/images.ts
@@ -4,12 +4,26 @@ import { getFile } from '../firebase'
 
 async function image(req: Request, res: Response) {
   const { image } = req.params
-  const file = await getFile(image)
-  const readStream = new stream.PassThrough()
+  try {
+    const file = await getFile(image)
+    const readStream = new stream.PassThrough()
 
-  readStream.end(file.buffer)
-  res.set('Content-Type', file.type)
-  readStream.pipe(res)
+    readStream.end(file.buffer)
+    res.set('Content-Type', file.type)
+    readStream.pipe(res)
+  } catch (err) {
+    if (err.code === 404) {
+      req.Inertia.setStatusCode(404).setViewData({ title: 'Page not found' }).render({
+        component: 'Errors/404',
+        props: {}
+      })
+    } else {
+      req.Inertia.setStatusCode(500).setViewData({ title: 'Something goes wrong' }).render({
+        component: 'Errors/500',
+        props: {}
+      })
+    }
+  }
 }
 
 export default [image]
