16685efac28fb7223fe2605b28a5604b0b37b324
Sergio Flores <byoigres@gmail.com>
2021-02-15 20:21:03 -0700

    Added Cache-Control header for images

--- src/routes/images.ts
+++ src/routes/images.ts
@@ -10,6 +10,14 @@ async function image(req: Request, res: Response) {
 
     readStream.end(file.buffer)
     res.set('Content-Type', file.type)
+    /**
+     * The max-age directive tells the browser how long it should cache the
+     * resource in seconds. This example sets the duration to 31536000, which
+     * corresponds to 1 year: 60 seconds × 60 minutes × 24 hours × 365 days
+     * = 31536000 seconds.
+     * @see https://web.dev/uses-long-cache-ttl/#:~:text=When%20a%20browser%20requests%20a,getting%20it%20from%20the%20network.
+     */
+    res.set('Cache-Control', 'max-age=31536000')
     readStream.pipe(res)
   } catch (err) {
     if (err.code === 404) {
