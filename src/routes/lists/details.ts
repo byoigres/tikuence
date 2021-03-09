1925b1effea83d931badcc57128976af46b47bf5
Sergio Flores <byoigres@gmail.com>
2021-03-08 21:27:15 -0700

    Added pagination

--- src/routes/lists/details.ts
+++ src/routes/lists/details.ts
@@ -1,15 +1,50 @@
-import { Request } from 'express'
+import { Request, Response, NextFunction } from 'express'
+import httpContext from 'express-http-context'
 import Knex, { Tables, iDetailsItem } from '../../utils/knex'
 
-async function getCounters (req: Request) {
-  const payload = req.params
+async function verifyParams(req: Request, res: Response, next: NextFunction) {
+  const query = req.query
+  const params = req.params
+  const isInertiaRequest = req.headers['x-inertia']
+  const pageSize = 3
+  let offset = 0
+  let page = 1
+
+  if (query.page && typeof query.page === 'string') {
+    // If a page is provided and is not an Inertia request,
+    // redirect to "/" without the page query param
+    if (isInertiaRequest === undefined) {
+      return req.Inertia.redirect(`/list/${params.listId}`)
+    }
+
+    // TODO: try-catch when `page` is not a number
+    page = parseInt(query.page, 10)
+
+    if (page <= 0) {
+      page = 1
+    }
+
+    offset = page * pageSize - pageSize
+  }
+
+  httpContext.set('page', page)
+  httpContext.set('pageSize', pageSize)
+  httpContext.set('offset', offset)
+
+  next()
+}
+
+async function getListVideos(req: Request) {
+  const pageSize = httpContext.get('pageSize')
+  const offset = httpContext.get('offset')
+  const params = req.params
 
   const knex = Knex()
 
   const list = await knex<iDetailsItem>(`${Tables.Lists} AS L`)
     .select('L.id', 'L.title', 'U.id AS user_id')
     .join(`${Tables.Users} AS U`, 'L.user_id', 'U.id')
-    .where('L.id', payload.id)
+    .where('L.id', params.listId)
     .first()
 
   if (!list) {
@@ -21,8 +56,10 @@ async function getCounters (req: Request) {
   const videos = await knex(`${Tables.ListsVideos} AS LV`)
     .select('LV.video_id AS id', 'V.tiktok_id', 'V.title', 'V.html')
     .join(`${Tables.Videos} AS V`, 'LV.video_id', 'V.id')
-    .where('LV.list_id', payload.id)
-    .orderBy('LV.order_id')
+    .where('LV.list_id', params.listId)
+    .orderBy('LV.order_id', 'ASC')
+    .limit(pageSize)
+    .offset(offset)
 
   req.Inertia.setViewData({ title: list.title }).render({
     component: 'Lists/List',
@@ -36,4 +73,4 @@ async function getCounters (req: Request) {
   })
 }
 
-export default [getCounters]
+export default [verifyParams, getListVideos]
