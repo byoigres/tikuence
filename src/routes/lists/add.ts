f8e54905f1bc3243e5996d64bc5f8f19baa34af4
Sergio Flores <byoigres@gmail.com>
2021-03-12 23:05:36 -0700

    Validated the referer to know what component to load

--- src/routes/lists/add.ts
+++ src/routes/lists/add.ts
@@ -1,9 +1,21 @@
 import { Request } from 'express'
-import { isAuthenticated } from '../../middlewares/inertia'
+import { isAuthenticated, getReferer } from '../../middlewares/inertia'
 
 function response (req: Request) {
+  const referer = getReferer(req)
+  let component = 'Feed'
+
+  /**
+   * By now only two pages load the Add New List component,
+   * if there are new pages to load this component validate
+   * the else from the following validation.
+   */
+  if (referer && referer.startsWith('/users/')) {
+    component = 'Profile/Profile'
+  }
+
   req.Inertia.setViewData({ title: 'Add new list' }).render({
-    component: 'Feed',
+    component,
     props: {
       showModal: 'add-list'
     }
