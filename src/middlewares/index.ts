bc4c3f9503d10e49cc12080049efef63b1bd59cc
Sergio Flores <byoigres@gmail.com>
2021-03-25 15:41:43 -0700

    Added connect-session-knex

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -7,6 +7,14 @@ import flash from 'connect-flash'
 import inertia, { populateSharedProps } from './inertia'
 import cookies from './cookies'
 import passport from './passport'
+import Knex from '../utils/knex'
+
+const KnexSessionStore = require('connect-session-knex')(session)
+
+const store = new KnexSessionStore({
+  knex: Knex(),
+  tablename: 'sessions' // optional. Defaults to 'sessions'
+})
 
 function middlewares(app: Express) {
   app.use(
@@ -26,7 +34,8 @@ function middlewares(app: Express) {
       cookie: {
         secure: false,
         maxAge: 60000 * 60
-      }
+      },
+      store
     })
   )
   app.use(passport.initialize())
