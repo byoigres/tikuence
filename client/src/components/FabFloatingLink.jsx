554bb720f3454102d646df12babd2bbb11f721c1
Sergio Flores <byoigres@gmail.com>
2021-04-09 11:02:44 -0600

    Added media query validation for Floating Action Button

--- client/src/components/FabFloatingLink.jsx
+++ client/src/components/FabFloatingLink.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
 import Fab from '@material-ui/core/Fab';
 import AddIcon from '@material-ui/icons/Add';
 
 const useStyles = makeStyles((theme) => ({
   container: {
     position: 'fixed',
-    bottom: theme.spacing(1),
+    bottom: theme.spacing(2),
     textAlign: 'right',
-    // height: 100,
-    // backgroundColor: 'red',
     [theme.breakpoints.up('sm')]: {
       width: theme.breakpoints.values.md,
     },
@@ -17,23 +16,20 @@ const useStyles = makeStyles((theme) => ({
       width: '100%',
     },
   },
-  fab: {
+  fab: ({ isMdPlus200Match }) => ({
+    right: isMdPlus200Match ? 0 : theme.spacing(2),
     position: 'absolute',
-    [theme.breakpoints.up('sm')]: {
-      right: 0,
-    },
-    [theme.breakpoints.between('xs', 'sm')]: {
-      right: theme.spacing(1),
-    },
-    bottom: '0',
-  },
+    bottom: 0,
+  }),
 }));
 
 const FabFloatingLink = ({ onClick }) => {
-  const classes = useStyles();
+  const theme = useTheme();;
+  const isMdPlus200Match = useMediaQuery(`(min-width:${theme.breakpoints.values.md + 200}px)`);
+  const classes = useStyles({ isMdPlus200Match });
 
   return (
-    <div className={classes.container} data-name="Fab-X">
+    <div className={classes.container}>
       <Fab color="primary" aria-label="add" className={classes.fab} onClick={onClick}>
         <AddIcon />
       </Fab>
