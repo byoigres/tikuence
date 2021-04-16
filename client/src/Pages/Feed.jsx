8cd209061bd430aec97bc564da1cce6eb1e66daa
Sergio Flores <byoigres@gmail.com>
2021-04-16 11:45:11 -0600

    Replaced Material List with Thumbnail List

--- client/src/Pages/Feed.jsx
+++ client/src/Pages/Feed.jsx
@@ -2,17 +2,10 @@ import React, { Fragment, useState, useEffect } from 'react';
 import { Inertia } from '@inertiajs/inertia';
 import { InertiaLink, usePage } from '@inertiajs/inertia-react';
 import { Waypoint } from 'react-waypoint';
-import Typography from '@material-ui/core/Typography';
-import MaterialList from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
-import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import BottomNavigation from '@material-ui/core/BottomNavigation';
 import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
-import Avatar from '@material-ui/core/Avatar';
 import Grid from '@material-ui/core/Grid';
-import Divider from '@material-ui/core/Divider';
 import RestoreIcon from '@material-ui/icons/Restore';
 import WbSunnyIcon from '@material-ui/icons/WbSunny';
 import { makeStyles } from '@material-ui/core/styles';
@@ -24,6 +17,7 @@ import Profile from './Profile/Profile';
 import List from './Lists/List';
 import Login from './Auth/Login';
 import EndOfList from '../components/EndOfList';
+import ThumbailList, { ThumbnailItem } from '../components/ThumbailList';
 
 const useStyles = makeStyles((theme) => ({
   list: {
@@ -45,6 +39,26 @@ const useStyles = makeStyles((theme) => ({
     margin: '1rem',
     fontStyle: 'italic',
   },
+  list_card: {
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    flexGrow: 1,
+  },
+  list_cardContent: {
+    paddingLeft: '0.5rem',
+    paddingRight: '0.5rem',
+    paddingTop: '1rem',
+    paddingBottom: 0,
+  },
+  list_actionArea: ({ isMe }) => ({
+    display: 'flex',
+    flexDirection: 'column',
+    flexGrow: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start',
+    paddingBottom: isMe ? '0' : '1rem',
+  }),
 }));
 
 const categories = [
@@ -105,7 +119,9 @@ const PageFeed = () => {
       <SEO title={categories[categoryIndex].pageTitle} />
       <Grid
         container
-        style={{ paddingLeft: '1rem', paddingRight: '1rem', backgroundColor: 'white' }}
+        style={{
+          backgroundColor: 'white',
+        }}
       >
         <Grid item xs={12} md={12}>
           <BottomNavigation
@@ -126,73 +142,35 @@ const PageFeed = () => {
               <BottomNavigationAction key={item.label} label={item.label} icon={item.icon} />
             ))}
           </BottomNavigation>
-          <MaterialList dense={false} className={classes.list}>
-            {lists &&
-              lists.map((item) => (
-                <Fragment key={`list-item-${item.id}`}>
-                  <ListItem
-                    key={item.id}
-                    component={InertiaLink}
-                    button
-                    href={`/list/${item.id}`}
-                    preserveScroll
-                    preserveState
-                    headers={{ 'X-Page-Referer': 'feed' }}
-                    only={['auth', 'flash', 'errors', 'modal', 'list', 'videos', 'referer']}
-                  >
-                    <ListItemAvatar className={classes.listItemAvatar}>
-                      <Avatar
-                        alt={item.title}
-                        className={classes.avatar}
-                        variant="square"
-                        src={item.thumbnail}
-                      />
-                    </ListItemAvatar>
-                    <ListItemText
-                      id={item.id}
-                      primary={
-                        <Typography component="strong" variant="h6" color="textPrimary">
-                          {item.title}
-                        </Typography>
-                      }
-                      secondary={
-                        <>
-                          <Typography
-                            component="strong"
-                            variant="subtitle2"
-                            color="textPrimary"
-                            style={{ display: 'block' }}
-                          >
-                            {`${item.total_videos} videos`}
-                          </Typography>
-                          <Typography component="span" variant="subtitle1" color="textPrimary">
-                            {`@${item.username}`}
-                          </Typography>
-                        </>
-                      }
-                    />
-                  </ListItem>
-                  <Divider variant="fullWidth" component="li" />
-                </Fragment>
-              ))}
-            {isTheEnd && <EndOfList text="You reached the end of the lists" />}
-            {!isTheEnd && (
-              <>
-                {!modal && (
-                  <div className={classes.loader}>
-                    <CircularProgress />
-                  </div>
-                )}
-                <Waypoint
-                  onEnter={() => {
-                    if (lists.length > 0) {
-                      setCurrentPage(currentPage + 1);
-                    }
-                  }}
+          <ThumbailList isMobile={isMobile}>
+            {lists.length !== 0 &&
+              lists.map((list) => (
+                <ThumbnailItem
+                  key={`thumbnail-item-${list.id}`}
+                  id={list.id}
+                  thumbnail={list.thumbnail}
+                  title={list.title}
+                  videos={list.total_videos}
                 />
-              </>
-            )}
-          </MaterialList>
+              ))}
+          </ThumbailList>
+          {isTheEnd && <EndOfList text="You reached the end of the lists" />}
+          {!isTheEnd && (
+            <>
+              {!modal && (
+                <div className={classes.loader}>
+                  <CircularProgress />
+                </div>
+              )}
+              <Waypoint
+                onEnter={() => {
+                  if (lists.length > 0) {
+                    setCurrentPage(currentPage + 1);
+                  }
+                }}
+              />
+            </>
+          )}
         </Grid>
       </Grid>
       {isAuthenticated && (
