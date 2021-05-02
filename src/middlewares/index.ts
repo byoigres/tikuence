d723f1084e6ce79ce0cf1d1a88c5fe155d72379b
Sergio Flores <byoigres@gmail.com>
2021-05-02 08:06:14 -0600

    Added xrcf protection

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -3,6 +3,8 @@ import httpContext from 'express-http-context'
 import session from 'express-session'
 import compression from 'compression'
 import flash from 'connect-flash'
+import cookieParser from 'cookie-parser'
+import csurf from 'csurf'
 
 import inertia, { populateSharedProps } from './inertia'
 import passport from './passport'
@@ -51,10 +53,24 @@ function middlewares(app: Express) {
       store
     })
   )
+  app.use(cookieParser())
   app.use(passport.initialize())
   app.use(passport.session())
   app.use(flash())
   app.use(inertia)
+  app.use(
+    csurf({
+      ignoreMethods: ['GET', 'HEAD', 'OPTIONS', 'POST', 'PUT', 'DELETE'],
+      cookie: {
+        key: 'XSRF-TOKEN'
+        // domain: process.env.NODE_ENV === 'production' ? config.get('/session/domain') : undefined,
+        // httpOnly: true,
+        // sameSite: false,
+        // secure: !!config.get('/session/secure'),
+        // maxAge: config.get('/session/maxAge')
+      }
+    })
+  )
   app.use(populateSharedProps)
   app.use(httpContext.middleware)
 }
