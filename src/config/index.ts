7e89f7573f6674e822101f3f5ce2ae46758a83bd
Sergio Flores <byoigres@gmail.com>
2024-08-16 20:23:29 -0600

    Clean up branch.

--- src/config/index.ts
+++
@@ -1,8 +0,0 @@
-import { Store } from 'confidence'
-import manifest from './manifest'
-
-const store = new Store()
-
-store.load(manifest)
-
-export default store
--- src/middlewares/cookies.ts
+++
@@ -1,5 +0,0 @@
-import cookieParser from 'cookie-parser'
-
-const cookies = cookieParser('my super secret code')
-
-export default cookies
